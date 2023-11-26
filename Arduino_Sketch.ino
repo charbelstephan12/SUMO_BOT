6c5f57d1eb7a810246eb1ea01faaca86792a346a
Charbel Estephan <150896427+charbelstephan12@users.noreply.github.com>
2023-11-26 08:11:26 +0200

    Update Arduino_Sketch.ino

--- Arduino_Sketch.ino
+++ Arduino_Sketch.ino
@@ -1,31 +1,41 @@
-long centimeters = 0;
+// this value should be changed after making some tests 
+long distanceToAttack=120;
 
+// motor pin recognision 
 int motorPin1 = 5;
 int motorPin2 = 6;
 int motorPin3 = 9;
 int motorPin4 = 10;
 
+// Utlrasonic sensor pins 
+long centimeters = 0; // this a variable used to calculate the distance using the ultrasonic sensor in centimeters 
+const int TrigPin = 12; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 11; // Echo Pin of the Ultrasonic Sensor 
+
+// enA & enB shouldn't be connected these are only for tinkercad simulation 
 int enA = 13;
 int enB = 8;
 
-const int pingPin = 12; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 11;
-
+// for frontSensor1Pin / frontSensor2Pin / backSensor1Pin / backSensor2Pin; We should only connect the analog pins and not the digital pins 
 const int frontSensor1Pin = A0;
 const int frontSensor2Pin = A1;
-
 const int backSensor1Pin = A2;
 const int backSensor2Pin = A3;
 
+// convert form microseconds to centimeters 
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+} 
 
+// mesure the distance using the Ultrasonic sensor 
 void ultra() {
   long duration, inches, cm;
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
+  pinMode(TrigPin, OUTPUT);
+  digitalWrite(TrigPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(TrigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(pingPin, LOW);
+  digitalWrite(TrigPin, LOW);
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
   centimeters = microsecondsToCentimeters(duration);
@@ -34,9 +44,8 @@ void ultra() {
   Serial.println();
   delay(100);
 }
-long microsecondsToCentimeters(long microseconds) {
-  return microseconds / 29 / 2;
-}
+
+// used when the frontSensors are activated 
 void backward(){
   digitalWrite(enA, HIGH);
   digitalWrite(enB, HIGH);
@@ -45,6 +54,7 @@ void backward(){
   digitalWrite(motorPin3, LOW);
   digitalWrite(motorPin4, HIGH);
 }
+// used when the backSensors are activated 
 void forward(){
   digitalWrite(enA, HIGH);
   digitalWrite(enB, HIGH);
@@ -53,7 +63,8 @@ void forward(){
   digitalWrite(motorPin3, HIGH);
   digitalWrite(motorPin4, LOW);
 }
-void turn(){
+// Search for enemy 
+void search(){
   digitalWrite(enA, HIGH);
   digitalWrite(enB, HIGH);
   digitalWrite(motorPin1, HIGH); 
@@ -61,25 +72,29 @@ void turn(){
   digitalWrite(motorPin3, LOW);
   digitalWrite(motorPin4, HIGH);
 }
-void detection(){
-  ultra();
-  if (centimeters < 120) {
+// the enemy was found, Hence we should attack 
+void attack(){
     digitalWrite(enA, HIGH);
     digitalWrite(enB, HIGH);
     digitalWrite(motorPin1, HIGH);
     digitalWrite(motorPin2, LOW);
     digitalWrite(motorPin3, HIGH);
     digitalWrite(motorPin4, LOW);
+}
+
+// this void merge both voids attack() and search()
+void detection(){
+  ultra();
+  if (centimeters < distanceToAttack) {
+    attack();
   }
   else {
-  digitalWrite(enA, HIGH);
-  digitalWrite(enB, HIGH);
-  digitalWrite(motorPin1, HIGH); 
-  digitalWrite(motorPin2, LOW);
-  digitalWrite(motorPin3, LOW);
-  digitalWrite(motorPin4, HIGH);  }
+    search();
+  }
   delay(100);
 }
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(motorPin1, OUTPUT);
@@ -95,6 +110,8 @@ void setup() {
   pinMode(backSensor1Pin, INPUT);
   pinMode(backSensor2Pin, INPUT);
 }
+
+
 void loop() {
   
   int frontSensor1Value = digitalRead(frontSensor1Pin);
@@ -102,18 +119,22 @@ void loop() {
   int backSensor1Value = digitalRead(backSensor1Pin);
   int backSensor2Value = digitalRead(backSensor2Pin);
 
+// Check for frontSensors 
   if (frontSensor1Value == 1 || frontSensor2Value == 1 ) {
 	backward();
     delay(5000);
   }
+// Check for backSensors 
   if (backSensor1Value == 1 || backSensor2Value == 1) {
     forward();
     delay(5000);
   }
 
+// All sensors aren't activated then we can start the detection 
   if (frontSensor1Value == 0 && frontSensor2Value == 0 &&
       backSensor1Value == 0 && backSensor2Value == 0) {
     detection();
   }
+
   delay(100);
 }
